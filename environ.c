c57aca664baac11a77218c44492685f376ca0cca
Rick-Reign <rickreign6740@gamil.com>
2023-02-22 17:10:48 -0800

    new environ

--- environ.c
+++ environ.c
@@ -1,5 +1,3 @@
-environ.c
-
 #include "shell.h"
 
 /**
@@ -13,7 +11,6 @@ int _myenv(info_t *info)
 	print_list_str(info->env);
 	return (0);
 }
-
 /**
  * _getenv - gets the value of an environ variable
  * @info: Structure containing potential arguments. Used to maintain
@@ -29,13 +26,13 @@ char *_getenv(info_t *info, const char *name)
 	while (node)
 	{
 		p = starts_with(node->str, name);
+
 		if (p && *p)
 			return (p);
 		node = node->next;
 	}
 	return (NULL);
 }
-
 /**
  * _mysetenv - Initialize a new environment variable,
  *             or modify an existing one
@@ -54,7 +51,6 @@ int _mysetenv(info_t *info)
 		return (0);
 	return (1);
 }
-
 /**
  * _myunsetenv - Remove an environment variable
  * @info: Structure containing potential arguments. Used to maintain
@@ -72,10 +68,8 @@ int _myunsetenv(info_t *info)
 	}
 	for (i = 1; i <= info->argc; i++)
 		_unsetenv(info, info->argv[i]);
-
 	return (0);
 }
-
 /**
  * populate_env_list - populates env linked list
  * @info: Structure containing potential arguments. Used to maintain
